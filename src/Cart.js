fccb9d1dcbc353d3179bc88355e842a90ed0db0e
Sarah Rockhill <sarahrockhill@Sarahs-MacBook-Pro.local>
2020-12-01 23:57:12 -0500

    stuff

--- src/Cart.js
+++ src/Cart.js
@@ -28,33 +28,35 @@ class Cart extends  React.Component {
                 </style>
 
                 <h4 className="text-center card-header"> My Cart </h4>
-                {Object.keys(this.props.cart).map(key  =>
-                    <div className="row p-2">
-                        <div className="col">
-                            <div className="text-center">
-                                {key} | $
-                                { + this.props.cart[key].item.price * this.props.cart[key].quantity}
+                <div className="card-body">
+                    {Object.keys(this.props.cart).map(key  =>
+                        <div className="row p-2">
+                            <div className="col">
+                                <div className="text-center">
+                                    {key} x {this.props.cart[key].quantity} | $
+                                    { + this.props.cart[key].item.price * this.props.cart[key].quantity}
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className=" text-center">
+                                    <Button variant="black-outline"
+                                            onClick={() => this.removeFromCart(this.props.cart[key].item)}>
+                                        -
+                                    </Button>
+                                    <Button
+                                            variant="black-outline"
+                                            onClick={() => this.addToCart(this.props.cart[key].item)}>
+                                        +
+                                    </Button>
+                                </div>
                             </div>
                         </div>
-                        <div className="col">
-                            <div className=" text-center">
-                                <Button variant="black-outline"
-                                        onClick={() => this.removeFromCart(this.props.cart[key].item)}>
-                                    -
-                                </Button>
-                                <Button
-                                        variant="black-outline"
-                                        onClick={() => this.addToCart(this.props.cart[key].item)}>
-                                    +
-                                </Button>
-                            </div>
-                        </div>
-                    </div>
 
-                )}
+                    )}
+                </div>
                 <div className="row">
                     <div className="col">
-                        <h6 className="text-center subtotal mt-3">
+                        <h6 className="text-center subtotal mt-1">
                             Subtotal: $
                             <span style={{fontWeight: "bold"}}>
                                 {Object.keys(this.props.cart).reduce((a, b) =>
